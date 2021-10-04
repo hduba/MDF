35d95fa2f2d1902ee504c8eef9732fa89c823e96
hduba <hduba@purdue.edu>
2021-10-04 19:46:55 -0400

    Removed duplicate patches

--- training/testdenoiser.py
+++ training/testdenoiser.py
@@ -3,6 +3,6 @@ import main_test_dncnn
 # Test prior trained on 9.75% noise on training image with 9.75% added noise
 main_test_dncnn.main()
 # Test prior trained on 2% noise on training image with 2% added noise
-main_test_dncnn.main(2, 2)
+main_test_dncnn.main(9.75, 2)
 # Test prior trained on 5% noise on training image with 5% added noise
 #main_test_dncnn.main(5, 5)
